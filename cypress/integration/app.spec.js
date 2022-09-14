098d1501ce2c02389bd93bbb35d8962f00c77a51
unknown <mohammedhossam7777@gmail.com>
2022-09-14 18:18:46 +0200

    adjust cypress spec to test

--- cypress/integration/app.spec.js
+++ cypress/integration/app.spec.js
@@ -1,6 +1,12 @@
+function add(n, m) {
+  return n + m;
+}
 describe("home page"),
   () => {
     it("", () => {
       cy.visit("http://localhost:3000/");
+    });
+    it("Example test", () => {
+      expect(add(1, 2)).to.eq(3);
     });
   };
