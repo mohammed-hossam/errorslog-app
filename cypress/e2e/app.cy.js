5ee494b775bb6c309f418716397a9b594f98ad8a
Mohammed Hossam <mohammedhossam7777@gmail.com>
2022-09-14 18:46:26 +0200

    CI setup after many adjustments commits
    * CI setup
    
    * Cypress adjuste uses version
    
    * adjust prettierignore file
    
    * adjust cypress command.ts file
    
    * adjust cypress spec to test
    
    * adjust cypress spec to test h1 tag
    
    * adjust cypress spec to test h1 tag after deleting addtional cypress files
    
    * change cypress test folderName from integration to e2e
    
    * change cypress test fileName from .spec.js to .cy.js
    
    * adjust cypress home page suite callback

---
+++ cypress/e2e/app.cy.js
@@ -0,0 +1,12 @@
+function add(n, m) {
+  return n + m;
+}
+describe("home page", () => {
+  it("test h1", () => {
+    cy.visit("http://localhost:3000/");
+    cy.get("h1").contains("Welcome to Next.js!");
+  });
+  it("Example test", () => {
+    expect(add(1, 2)).to.eq(3);
+  });
+});
