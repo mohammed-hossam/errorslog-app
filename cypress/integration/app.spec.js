610a735fb75cce89756497bc71c07b57d3081a2a
unknown <mohammedhossam7777@gmail.com>
2022-09-14 18:26:12 +0200

    adjust cypress spec to test h1 tag

--- cypress/integration/app.spec.js
+++ cypress/integration/app.spec.js
@@ -3,8 +3,9 @@ function add(n, m) {
 }
 describe("home page"),
   () => {
-    it("", () => {
+    it("test h1", () => {
       cy.visit("http://localhost:3000/");
+      cy.get("h1").contains("Welcome to Next.js!");
     });
     it("Example test", () => {
       expect(add(1, 2)).to.eq(3);
