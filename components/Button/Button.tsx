cdf10f1bd1d3a8d0d8ebafd854036ddfa76d1d46
Mohammed Hossam <mohammedhossam7777@gmail.com>
2022-10-01 13:26:40 +0200

    Mobile sidebar (#8)
    * setup sidebar and add normalizecss
    
    * close cypress temporary
    
    * finishig sidebar for the desktop version with styledComponents theme and storybook
    
    * remove eslintcashe
    
    * sidebar mobile version and add context to desktop version testing

---
+++ components/Button/Button.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Button = styled.button`
+  cursor: pointer;
+  //reset some default button css
+  background-color: transparent;
+  border: none;
+  line-height: normal;
+  margin: 0;
+  padding: 0;
+`;
--- cypress/e2e/sidebarNavigation.spec.ts
+++ cypress/e2e/sidebarNavigation.spec.ts
@@ -6,33 +6,39 @@ describe("Sidebar Navigation", () => {
     cy.visit("http://localhost:3000");
   });
 
-  it("links are working", () => {
-    // cy.visit("http://localhost:3000/");
-    // cy.get("h1").contains("Welcome to Next.js!");
-    // check that each link leads to the correct page
-    cy.get("nav").contains("Issues").click();
-    // cy.url().should("eq", "http://localhost:3000/dashboard/issues");
-    cy.url().should("include", "issues");
-  });
+  context("desktop resolution", () => {
+    beforeEach(() => {
+      cy.viewport(1025, 900);
+    });
 
-  it("is collapsible", () => {
-    // collapse navigation
-    cy.get("nav").contains("Collapse").click();
+    it("links are working", () => {
+      // cy.visit("http://localhost:3000/");
+      // cy.get("h1").contains("Welcome to Next.js!");
+      // check that each link leads to the correct page
+      cy.get("nav").contains("Issues").click();
+      // cy.url().should("eq", "http://localhost:3000/dashboard/issues");
+      cy.url().should("include", "issues");
+    });
 
-    // check that links still exist and are functionable
-    cy.get("nav").find("a").should("have.length", 5).eq(2).click();
-    cy.url().should("eq", "http://localhost:3000/dashboard/alerts");
+    it("is collapsible", () => {
+      // collapse navigation
+      cy.get("nav").contains("Collapse").click();
 
-    // check that text is not rendered
-    cy.get("nav").contains("Alerts", { timeout: 0 }).should("not.exist");
+      // check that links still exist and are functionable
+      cy.get("nav").find("a").should("have.length", 5).eq(2).click();
+      cy.url().should("eq", "http://localhost:3000/dashboard/alerts");
 
-    //without collapsing
-    // cy.get("nav").contains("Alerts", { timeout: 0 }).should("exist");
-    // https://github.com/cypress-io/cypress/issues/7680#issuecomment-643059326
-  });
+      // check that text is not rendered
+      cy.get("nav").contains("Alerts", { timeout: 0 }).should("not.exist");
 
-  it("Example test", () => {
-    expect(add(1, 2)).to.eq(3);
+      //without collapsing
+      // cy.get("nav").contains("Alerts", { timeout: 0 }).should("exist");
+      // https://github.com/cypress-io/cypress/issues/7680#issuecomment-643059326
+    });
+
+    it("Example test", () => {
+      expect(add(1, 2)).to.eq(3);
+    });
   });
 });
 
