b62659bbeb6d8efe2243debb5265498883a0c61c
unknown <mohammedhossam7777@gmail.com>
2022-10-01 18:29:21 +0200

    sidebar mobile version testing

--- cypress/e2e/sidebarNavigation.spec.ts
+++ cypress/e2e/sidebarNavigation.spec.ts
@@ -40,6 +40,27 @@ describe("Sidebar Navigation", () => {
       expect(add(1, 2)).to.eq(3);
     });
   });
+
+  context.only("mobile resolution", () => {
+    beforeEach(() => {
+      cy.viewport("iphone-8");
+    });
+    /* Cypress actually verifies that element is hidden via css property like display: none or visibility: hidden. But in our case, the element we are trying to assert is not even present in our app. That is why our assertion fails. Instead of visibility check, we should be doing an assertion of non-existence, so .should('not.exist') */
+    it("sidebar navigation not appearing in the beginning", () => {
+      cy.wait(500); // wait for animation to finish
+      cy.get("header").then(() => {
+        cy.get("nav").isNotWithinViewport();
+      });
+    });
+
+    it("toggles sidebar navigation by clicking the menu icon", () => {
+      // open mobile navigation
+      cy.get("header").find("img[alt='open menu']").click();
+
+      cy.wait(500); // wait for animation to finish
+      cy.get("nav").isWithinViewport();
+    });
+  });
 });
 
 export {};
